3cb699602b22a37b33f46109605431d9cd0fd07b
Massimo Lauria <lauria.massimo@gmail.com>
2011-08-30 00:09:09 +0200

    Fixing some configuration for MacOSX ls command

--- zsh.d/S80_dircolors.zsh
+++ zsh.d/S80_dircolors.zsh
@@ -2,8 +2,9 @@
 
 
 # Here the colors for ls
-if test -x =dircolors; then
+if [[ -f `which dircolors 2>/dev/null` ]]; then
     eval "`dircolors`"
     export LS_COLORS="*.ogm=01;35:${LS_COLORS}"
+    alias ls="ls --color=auto"
 fi
 

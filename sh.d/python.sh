80d0c7d05541047d3a6fec96ac64b1cf903608b8
Massimo Lauria <lauria.massimo@gmail.com>
2015-02-10 01:39:28 +0100

    Configuration for python tool: virtualenvwrapper
    This tool allows to manage different python installations for difference
    projects.

--- sh.d/python.sh
+++ sh.d/python.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
-# Copyright (C) 2011, 2012, 2013 by Massimo Lauria
+# Copyright (C) 2011, 2012, 2013, 2015 by Massimo Lauria
 #
 # Created   : "2011-09-22, Thursday 01:06 (CEST) Massimo Lauria"
-# Time-stamp: "2013-12-01, 03:35 (CET) Massimo Lauria"
+# Time-stamp: "2015-02-10, 01:36 (CET) Massimo Lauria"
 #
 # Description::
 #
@@ -33,6 +33,12 @@ export IPYTHONDIR
 DOT_SAGE=$HOME/.sage/
 export DOT_SAGE
 
+
+# Setup virtual envs
+export WORKON_HOME=$HOME/.virtualenvs
+if type virtualenvwrapper_lazy.sh > /dev/null; then
+    source `which virtualenvwrapper_lazy.sh`
+fi
 
 # Local Variables:
 # fill-column: 80

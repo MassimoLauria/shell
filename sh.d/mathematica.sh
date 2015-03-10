3b1849ed607f976f7e4a24c63272a1924f145a22
Massimo Lauria <lauria.massimo@gmail.com>
2015-03-10 10:57:48 +0100

    Mathematica shell on Linux platforms.

--- sh.d/mathematica.sh
+++ sh.d/mathematica.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-mathpath=/Applications/Mathematica.app/Contents/MacOS/MathKernel
-
-if [ -x $mathpath  ]; then
-    if type rlwrap >/dev/null; then
-        alias math="rlwrap $mathpath"
-    else
-        alias math=$mathpath
-    fi 
+if [ -x /Applications/Mathematica.app/Contents/MacOS/MathKernel ]; then
+    mathpath=/Applications/Mathematica.app/Contents/MacOS/MathKernel
+elif [ -x /pkg/mathematica/10.0.2/os/bin/MathKernel ]; then
+    mathpath=/pkg/mathematica/10.0.2/os/bin/MathKernel
+elif [ -x /usr/local/bin/math ]; then
+    mathpath=/usr/local/bin/math
+else
+    mathpath=math
 fi
+
+if type rlwrap >/dev/null; then
+    alias math="rlwrap $mathpath"
+else
+    alias math=$mathpath
+fi 

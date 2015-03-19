66f93867e14172cadcbad8072d9b0e10b9d0c521
Massimo Lauria <lauria.massimo@gmail.com>
2015-03-19 17:53:39 +0100

    File opening from Linux.

--- zsh.d/S30_alias.zsh
+++ zsh.d/S30_alias.zsh
@@ -3,7 +3,7 @@
 # Copyright (C) 2011, 2014, 2015 by Massimo Lauria
 #
 # Created   : "2011-11-17, Thursday 20:43 (CET) Massimo Lauria"
-# Time-stamp: "2015-03-18, 02:20 (CET) Massimo Lauria"
+# Time-stamp: "2015-03-19, 17:52 (CET) Massimo Lauria"
 #
 # Description::
 #
@@ -20,15 +20,28 @@ alias scp="noglob scp"
 alias dirs="dirs -v"
 
 
-if [ `uname` = "Linux" ]; then
-    alias open=xdg-open
-fi
 
 function o {
-    if [ $# -lt 1 ]; then
-        open .
+    if [ `uname` = "Linux" ]; then
+
+        # by default open current folder
+        if [ $# -lt 1 ]; then
+            nohup pcmanfm .  </dev/null &>/dev/null 
+        elif [ -d "$1" ]; then
+            nohup pcmanfm $1 </dev/null &>/dev/null
+        else
+            nohup mimeopen $1 </dev/null &>/dev/null
+        fi
+    elif [ `uname` = "Darwin"]; then
+
+        if [ $# -lt 1 ]; then
+            open .
+        else
+            open $@
+        fi
+
     else
-        open $@
+        echo "Not implemented on this platform"
     fi
 }
 

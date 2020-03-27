8013bc8eb46ab2b8ff0c6f444c33bc10d2ad0635
Massimo Lauria <lauria.massimo@gmail.com>
2020-03-27 02:48:39 +0100

    Setup for fzf

--- sh.d/utils.sh
+++ sh.d/utils.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
 #
-# Massimo Lauria setup for cmd line utils
+# Setup for command line tools:
+#
+# There are very neat and powerful tools that extends the usual
+# command line utilities. I will set them up, when present.
 #
 
-
-# {{{ Pager --------------------------------------------------------------------
+#
+# PAGER setup. I prefer http://www.jedsoft.org/most/
+#
 if type most >/dev/null 2>&1; then
     PAGER=most
 elif type less >/dev/null 2>&1; then
@@ -14,10 +18,20 @@ else
     PAGER=more
 fi
 export PAGER
-# }}} --------------------------------------------------------------------------
 
-# bat is better than cat, but defaults are funny
+
+# 'bat' is good for showing files
+# so I use it instead of 'cat'
+# https://github.com/sharkdp/bat
 if type bat >/dev/null 2>&1; then
     export BAT_PAGER=less
     alias cat=bat
 fi
+
+# 'exa' is a more sophisticated replacement for 'ls'
+# https://the.exa.website/
+if type exa >/dev/null 2>&1; then
+    alias ls="exa -hg --git --git-ignore"
+fi
+
+
---
+++ zsh.d/S90_fzf.zsh
@@ -0,0 +1,58 @@
+#!/bin/zsh
+#
+# fzf - Fuzzy Finder for the command line
+#
+# Install via Homebrew on MacOS
+# $ brew install fzf  
+# $ $(brew --prefix)/opt/fzf/install
+#
+# Install via Git
+# $ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+# $ ~/.fzf/install
+#
+#
+
+# If ripgrep is installed, use this to search files in fzf
+# https://github.com/BurntSushi/ripgrep
+#
+# if type rg >/dev/null 2>&1; then
+#     export FZF_DEFAULT_COMMAND='rg --files'
+# fi
+
+
+export FZF_DEFAULT_OPTS="--border"
+
+### Files search
+export FZF_CTRL_T_OPTS="--prompt 'Files> ' -m --header='Select file(s)' --preview 'bat --style=numbers --color=always {} | head -500'"
+
+### Folder search
+export FZF_ALT_C_OPTS="--prompt 'Dir> ' -m --header='Select a folder' --preview 'ls -C {}' --preview-window hidden --bind '?:toggle-preview'"
+
+# Full command on the preview windows
+export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
+
+function fzf_activate() {
+    source ~/.fzf.zsh
+}
+
+function fzf_zsh_special_keys() {
+
+    # Search backward/forward in history
+    bindkey "^[b" fzf-history-widget  # M-b
+    bindkey "^[n" fzf-history-widget  # M-n
+
+    # "page up" and "page down" to search the history
+    bindkey "\e[5~" fzf-history-widget
+    bindkey "\e[6~" fzf-history-widget
+
+    # Ctrl-S search forward in history, but it is seldom useful
+    bindkey "^s" fzf-history-widget
+}
+
+# All setups for fzf
+function fzf_setup() {
+    fzf_activate
+    fzf_zsh_special_keys
+}
+
+[ -f ~/.fzf.zsh ] && fzf_setup

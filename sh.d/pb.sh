43a4327a4e18e7997203c76c711f3a13d3483ad3
Massimo Lauria <lauria.massimo@gmail.com>
2020-03-28 02:44:55 +0100

    Improvements to pb command

--- sh.d/pb.sh
+++ sh.d/pb.sh
@@ -15,13 +15,19 @@
 # - pinboard.py python package (https://github.com/lionheart/pinboard.py)
 # - fzf         fuzzy finder   (https://github.com/junegunn/fzf)
 
+# Possible options for fzf (color theme)
+# --color fg:242,bg:236,hl:65,fg+:15,bg+:239,hl+:108 \
+# --color info:108,prompt:109,spinner:108,pointer:168,marker:168 \
+
 
 PIN_CACHE_FILE=$HOME/.pinboard.cache
 PINBOARDRC=$HOME/.pinboardrc
 PINDOWNLOAD=${CONFIGDIR}/shell/sh.d/pincache.py
 
 
-function pbcache() {
+
+
+function pbcache() { 
     echo "Fetching pinboard.in bookmarks"
     python "$PINDOWNLOAD" --forcecolors > "$PIN_CACHE_FILE"
 }
@@ -33,9 +39,11 @@ function pb() {
         open_cmd=open
     fi
     cat ${PIN_CACHE_FILE}                       |
-        fzf -e --ansi --reverse -m \
-            --preview-window down:6:hidden:wrap --bind '?:toggle-preview' \
-            --preview 'echo {} | tr "\t" "\n" '  |
+        fzf -e -i --ansi --reverse -m \
+            --tabstop=4  -d '\t' \
+            --prompt='Pin> ' \
+            --preview-window down:6:wrap --bind '?:toggle-preview' \
+            --preview 'echo {1} "--- "{2}"\n\n"{3}"\n\nURL: "{5}'  |
         awk 'BEGIN { FS = "\t" } { print $5 }'  |
         xargs open
 }

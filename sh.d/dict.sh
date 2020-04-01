0f1973cd3f7d27be374c1fcc29d9f2bde04bc325
Massimo Lauria <lauria.massimo@gmail.com>
2020-04-02 00:48:14 +0200

    Dictionary search in shell

---
+++ sh.d/dict.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#
+# Massimo Lauria, 2020
+#
+# Shell utilities for dictionaries
+#
+#
+# d - interactively search for a word in the dictionary
+#
+
+#
+# Requirements:
+#
+# - fzf         fuzzy finder   (https://github.com/junegunn/fzf)
+# - sdcv        StarDict Console Version
+
+WORDFILE=$HOME/personal/dictionaries/wordlists/english_and_italian.txt
+
+    
+function d() {
+    local args=""
+    local query=""
+
+    if [ "$#" -ge 1 ]; then
+        query="-q"
+        args=$@
+    fi
+
+    cat $WORDFILE |
+    fzf -i \
+        $query $args \
+        --prompt "Cerca: " \
+        --preview 'sdcv -enc0 {}' \
+        --preview-window wrap  --bind '?:toggle-preview' | xargs -n 1 sdcv -enc0  
+}

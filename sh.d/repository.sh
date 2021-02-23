1b6543f0af7cad938894468b0997bccef4da6ba6
Massimo Lauria <lauria.massimo@gmail.com>
2021-02-23 03:59:17 +0100

    Shorter repository summary

--- sh.d/repository.sh
+++ sh.d/repository.sh
@@ -12,22 +12,22 @@
 function myrepos() {
     MAINREPOS=(~/config ~/personal ~/lavori)
     COMMANDS=$@
-    
+
     if [[ -z "$@" ]]; then
         COMMANDS=status
     fi
-    
+
     for p in ${MAINREPOS[@]}; do
         echo "Executing '$COMMANDS' on repository $p"
         pushd $p
         git  $COMMANDS
         git  submodule foreach git $COMMANDS
-        popd 
+        popd
     done
     }
 
 
-alias st="myrepos status"
+alias st="myrepos st"
 alias push="myrepos push"
 alias pull="myrepos pull"
 alias gsync="myrepos pull && myrepos push"

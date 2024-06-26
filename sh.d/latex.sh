e1e7c3f48ae138c8672002dac1452d969fb2b21f
Massimo Lauria <massimo.lauria@uniroma1.it>
2024-06-26 22:40:30 +0200

    Latex setup cleaned up

--- sh.d/latex.sh
+++ sh.d/latex.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
-# Copyright (C) 2014, 2015, 2017, 2020, 2023 by Massimo Lauria
+# Copyright (C) 2014, 2015, 2017, 2020, 2023, 2024 by Massimo Lauria
 #
 # Created   : "2014-10-22, Wednesday 13:06 (CEST) Massimo Lauria"
-# Time-stamp: "2024-05-14, 15:19 (CEST) Massimo Lauria"
+# Time-stamp: "2024-06-26, 22:30 (CEST) Massimo Lauria"
 #
 # Description::
 #
@@ -11,37 +11,11 @@
 #
 
 # LaTeX enviroment variables
+TEXEDIT="$EDITOR +%d %s"
+TEXMFHOME="$HOME/lavori/latex:"  # Private texmf tree
 
-# TEXEDIT="$EDITOR +%d %s"
+export TEXINPUTS=$(kpsepath tex):$HOME/lavori/latex
+export BIBINPUTS=$(kpsepath bib):$HOME/lavori/latex
 
-# TEXMFHOME="$HOME/lavori/latex:"  # Private texmf tree
-# if type sage >/dev/null 2>&1; then
-#     # Sage TeX packages
-#     TEXMFHOME="`sage -root`/local/share/texmf:$TEXMFHOME"
-# fi
-# export TEXEDIT
-# export TEXMFHOME
-
-
-
-# Additional paths for LaTeX resources when working on Jakob's papers.
-# it requires `function` capabilities from the shell.
-[ -n "$BASH" ] || [ -n "$ZSH_NAME" ] || return 0
-
-
-
-function jakobtex-enable {
-    local aPath=$HOME/lavori/latex/jakobtex
-    TEXINPUTS=$aPath:`kpsewhich --var-value TEXINPUTS`
-    BIBINPUTS=$aPath:`kpsewhich --var-value BIBINPUTS`
-    BSTINPUTS=$aPath:`kpsewhich --var-value BSTINPUTS`
-    export TEXINPUTS
-    export BIBINPUTS
-    export BSTINPUTS
-}
-
-function jakobtex-disable {
-    unset TEXINPUTS
-    unset BIBINPUTS
-    unset BSTINPUTS
-}
+export TEXEDIT
+export TEXMFHOME

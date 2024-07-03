8d5826490ace9c8067eade0b2e356ef04edb3d80
Massimo Lauria <massimo.lauria@uniroma1.it>
2024-07-03 23:14:18 +0200

    Simplifying my prompt. Just a little.

--- zsh.d/S90_prompt.zsh
+++ zsh.d/S90_prompt.zsh
@@ -115,17 +115,17 @@ esac
 # %a - action (e.g. rebase-i)
 # %R - repository path
 # %S - path in the repository
-# %s - repository type
+# %s - repository type ----> force git symbol ±
 PR_RST="%{${reset_color}%}"
-FMT_BRANCH="${PR_VCS_TYPE_COLOR}%s${PR_RST}:${PR_VCS_BRANCH_COLOR}%b%u%c${PR_RST}" # e.g. git:master¹²
+FMT_BRANCH="${PR_VCS_TYPE_COLOR}±${PR_RST}${PR_VCS_BRANCH_COLOR}%b%u%c${PR_RST}" # e.g. ±master¹²
 FMT_ACTION="(${PR_VCS_ACTION_COLOR}%a${PR_RST}%)"   # e.g. (rebase-i)
 FMT_PATH_VCS="${PR_RST}${PR_REPO_PATH_COLOR}%R${PR_REPO_SUBDIR_COLOR}/%S"   # e.g. ~/repo/subdir
 FMT_PATH_NVCS="${PR_RST}${PR_PATH_COLOR}%~${PR_RST}"   # e.g. ~/repo/subdir
 FMT_UNSTAGE_DIRTY="${PR_VCS_DIRTY_COLOR}¹${PR_RST}"
 FMT_STAGE_DIRTY="${PR_VCS_DIRTY_COLOR}²${PR_RST}"
 
-FMT_BL=$PR_BARS_COLOR$PR_SHIFT_IN$PR_HBAR$PR_SHIFT_OUT'['$PR_RST
-FMT_BR=$PR_BARS_COLOR']'$PR_SHIFT_IN$PR_HBAR$PR_SHIFT_OUT$PR_RST
+FMT_BL=$PR_BARS_COLOR'['$PR_RST
+FMT_BR=$PR_BARS_COLOR']'$PR_RST
 
 # check-for-changes can be really slow.
 # you should disable it, if you work with large repositories
@@ -141,8 +141,13 @@ zstyle ':vcs_info:*:prompt:*' nvcsformats   ""
 
 # {{{ PROMPT THEMED COMPONENTS -------------------------------------------------
 # Common
+
 exit_value=""
-exit_value_prompt='%(?.'$PR_EXITVALUE_T_COLOR'.'$PR_EXITVALUE_F_COLOR')%?'$PR_RESET
+
+exit_value_prompt=$PR_SHIFT_IN$PR_BARS_COLOR'['$PR_RESET\
+'%(?.'$PR_EXITVALUE_T_COLOR'.'$PR_EXITVALUE_F_COLOR')%?'\
+$PR_BARS_COLOR']'$PR_SHIFT_OUT$PR_RESET
+
 date_text=""
 time_prompt=$PR_TIME_COLOR'${date_text}'$PR_RESET
 
@@ -170,7 +175,7 @@ branch_prompt='$vcs_info_msg_0_'
 #
 # See: https://github.com/zsh-users/zsh/commit/4105f79a3a9b5a85c4ce167865e5ac661be160dc
 #
-#pwd_prompt='${${vcs_info_msg_1_%%.}/${HOME}/~}'  - Original - M.L.
+# pwd_prompt='${${vcs_info_msg_1_%%.}/${HOME}/~}' # - Original - M.L.
 #
 pwd_prompt='${${${vcs_info_msg_1_%%.}:-${PR_RST}${PR_PATH_COLOR}$PWD${PR_RST}}/${HOME}/~}'     # my fix - M.L.
 pwd_prompt_truncated="%B%40<..<${pwd_prompt}%<<%b"
@@ -196,69 +201,16 @@ function zsh_update-date_precmd {
 }
 precmd_functions+='zsh_update-date_precmd'
 
-# COMPUTE THE PADDING ----------------------------------------------
-
-
-function zsh_update_2nd_row_padding {
-    prompt_mid_filler="${(r.(($COLUMNS - 3)).. .)}"
-}
-precmd_functions+='zsh_update_2nd_row_padding'
-
-# FIXME: the occurrence of '─' in the last line should really be
-# $PR_HBAR instead.
-#
-function zsh_update_3rd_row_padding {
-    local zero='%([BSUbfksu]|([FK]|){*})'
-    local vcs_info_size=${#${(S%%)vcs_info_msg_0_//$~zero/}}
-    local ps1_first_line_size=${#${(S%%)ps1_first_line_unfilled//$~zero/}}
-    local date_size=${#${(S%%)time_prompt//$~zero/}}
-    if [ "$vcs_info_size" -gt 0 ];then
-        vcs_info_size=$((vcs_info_size-3))
-    fi
-    total_size=$((ps1_first_line_size + date_size + vcs_info_size))
-    prompt_top_filler="${(r.(($COLUMNS +16 - $total_size))..─.)}"
-}
-precmd_functions+='zsh_update_3rd_row_padding'
-
-# HACK: I compute the additional bars to pad the exit code at the very
-# last moment in prompt evaluation, using this function. I cannot get
-# zsh to compute it in the `zsh_update_1st_row_padding' function
-#
-function compensate_exit_length {
-    if [ $1 -lt 10 ]; then
-        echo $PR_HBAR$PR_HBAR
-    elif [ $1 -lt 100 ]; then
-        echo $PR_HBAR
-    else
-        echo ""
-    fi
-}
-
 
 # THE PROMPT MAIN COMPONENTS -------------------------------------------------
 
-ps1_first_line_unfilled=$PR_SHIFT_IN$PR_BARL_COLOR$PR_ULCORNER$PR_HBAR$PR_HBAR\
-$PR_BARS_COLOR$PR_HBAR$PR_SHIFT_OUT'('$PR_RESET\
-$time_prompt\
-$PR_BARS_COLOR')'$PR_SHIFT_IN$PR_HBAR$PR_RESET\
-$PR_BARL_COLOR$PR_HBAR$PR_HBAR\
-$PR_BARS_COLOR$PR_HBAR$PR_SHIFT_OUT'<'$PR_RESET\
-$exit_value_prompt\
-$PR_BARS_COLOR'>'$PR_SHIFT_IN$PR_HBAR$PR_RESET\
-$PR_BARL_COLOR$PR_HBAR$PR_SHIFT_OUT$PR_RESET\
+ps1_info_line=$PR_BARS_COLOR'('$PR_RESET$time_prompt$PR_BARS_COLOR')'$PR_RESET\
+$PR_BARL_COLOR$PR_SHIFT_IN$PR_HBAR$PR_HBAR$PR_SHIFT_OUT\
 $branch_prompt\
 $PR_BARL_COLOR$PR_SHIFT_IN$PR_HBAR$PR_HBAR$PR_SHIFT_OUT\
-$PR_BARS_COLOR$PR_SHIFT_IN$PR_HBAR$PR_SHIFT_OUT'('$PR_RESET\
+$PR_BARS_COLOR'('$PR_RESET\
 $pwd_prompt_truncated\
-$PR_BARS_COLOR')'\
-$PR_SHIFT_IN$PR_HBAR$PR_SHIFT_OUT$PR_RESET
-
-ps1_first_line=$ps1_first_line_unfilled$PR_BARL_COLOR$PR_SHIFT_IN'${prompt_top_filler}$(compensate_exit_length $?)'$PR_URCORNER$PR_SHIFT_OUT$PR_RESET
-
-ps1_second_line=$PR_BARL_COLOR$PR_SHIFT_IN$PR_VBAR$PR_SHIFT_OUT$PR_RESET'${prompt_mid_filler}'$PR_BARL_COLOR$PR_SHIFT_IN$PR_VBAR$PR_SHIFT_OUT$PR_RESET
-#ps1_second_line=$PR_BARL_COLOR$PR_SHIFT_IN$PR_VBAR$PR_SHIFT_OUT$PR_RESET
-
-ps1_third_line=$PR_SHIFT_IN$PR_BARL_COLOR$PR_LLCORNER$PR_HBAR$PR_SHIFT_OUT$PR_RESET$prompt_char_prompt' '
+$PR_BARS_COLOR')'$PR_RESET
 
 
 
@@ -269,21 +221,11 @@ case $TERM in
         PS2=$PR_PARSER_DATA_COLOR'    %_ '$PR_RESET$PR_PARSER_PROMPT_COLOR'→ '$PR_RESET
 
         PROMPT='
-'$ps1_first_line'
-'$ps1_second_line'
-'$ps1_third_line
+'$ps1_info_line'
 
-        RPROMPT=$PR_BARS_COLOR'<'$PR_RESET\
-$userhost_prompt\
-$PR_BARS_COLOR'>'$PR_RESET\
-$pyenv_prompt\
-$PR_BARL_COLOR$PR_SHIFT_IN$PR_HBAR$PR_LRCORNER$PR_SHIFT_OUT$PR_RESET # prompt for right side of screen
-        ;;
+'$exit_value_prompt' '$prompt_char_prompt' '
 
-    eterm*)
-        PROMPT=$PR_PATH_COLOR'%~'$PR_RESET'$ '
-        RPROMPT='['$exit_value_prompt']'$branch_prompt'('$time_prompt')' # prompt for right side of screen
-        PS2='> '
+        RPROMPT=$pyenv_prompt' '
         ;;
 
     dumb) # Simple prompt for dumb terminals

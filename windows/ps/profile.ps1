faa74c77d3784e63931f726bdb9041ae66c63e4e
Massimo Lauria <massimo.lauria@uniroma1.it>
2025-04-29 12:41:12 +0200

    Removing old useless stuff

--- windows/ps/profile.ps1
+++
@@ -1,9 +0,0 @@
-
-# Add my script dir to path
-$scriptdir = $env:home + "\config\shell\windows\ps\";
-
-$env:path           += ";" + $scriptdir;
-$env:PSModulePath   += ";" + $scriptdir;
-
-
-$MaximumHistoryCount = 150

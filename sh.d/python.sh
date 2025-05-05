5d84f3f4a1a72ff0f206808d38d550b75129a329
Massimo Lauria <massimo.lauria@uniroma1.it>
2025-05-05 17:20:52 +0200

    Python cofigured via inputrc

--- sh.d/python.sh
+++ sh.d/python.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
-# Copyright (C) 2011, 2012, 2013, 2015, 2017, 2018, 2020, 2021, 2024 by Massimo Lauria
+# Copyright (C) 2011, 2012, 2013, 2015, 2017, 2018, 2020, 2021, 2024, 2025 by Massimo Lauria
 #
 # Created   : "2011-09-22, Thursday 01:06 (CEST) Massimo Lauria"
-# Time-stamp: "2024-07-27, 15:58 (CEST) Massimo Lauria"
+# Time-stamp: "2025-05-05, 17:17 (CEST) Massimo Lauria"
 #
 # Description::
 #
@@ -13,11 +13,6 @@
 # Code::
 
 # Python enviroment variables
-PYTHONSTARTUP=$CONFIGDIR/python/pythonrc.py
-if [ -f $PYTHONSTARTUP ]; then
-    export PYTHONSTARTUP
-fi
-
 IPYTHONDIR=$HOME/.ipython/
 export IPYTHONDIR
 

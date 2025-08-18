e3a0536b35c712657c8a1df39934aea01eb12c34
Massimo Lauria <massimo.lauria@uniroma1.it>
2025-08-19 00:11:39 +0200

    Flatpak local path

--- sh.d/paths.sh
+++ sh.d/paths.sh
@@ -4,13 +4,16 @@
 #
 # Setup additional paths
 
-PATH=/var/lib/flatpak/exports/bin/:$PATH   # Flatpak
+# Flatpak local and global
+PATH=$HOME/.local/share/flatpak/exports/bin/:$PATH
+PATH=/var/lib/flatpak/exports/bin/:$PATH
 
 # alternative installation paths
 PATH=/opt/local/sbin:/opt/local/bin:$PATH
 PATH=/usr/local/sbin:/usr/local/bin:$PATH
 PATH=$HOME/.local/sbin:$HOME/.local/bin:$PATH
 PATH=$HOME/Library/bin:$PATH
+
 
 # programming environment stuff
 PATH=$HOME/.elan/bin:$PATH
